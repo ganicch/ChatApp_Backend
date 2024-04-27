79eb0733c5d8f3fc8ad568cb8eea0721f4d81f76
ganicchGit <edinganic99@gmail.com>
2024-04-27 20:02:29 +0200

    GroupChat_and_messages

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-const express = require("express")
-
+const express = require("express");
 const app = express();
 const PORT = 3001;
+const http = require('http');
+const socketIo = require('socket.io');
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+const server = http.createServer(app);
+var io = new socketIo.Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
 });
 
-app.listen(PORT, () => {
+let messages = [];
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  socket.emit('allMessages', messages);
+
+  socket.on('message', (message) => {
+    console.log('Received message:', message);
+    messages.push(message); 
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+app.get("/", (req, res) => {
+ res.send("hello")
+});
+
+server.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}/`);
 });
